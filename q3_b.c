cd7b4430f97290d01b6eaa291c6d5a5b87a2a97d
sheron jude <53206494+sheron184@users.noreply.github.com>
2020-12-17 10:53:47 +0530

    Delete a specific integer from an array

---
+++ q3_b.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+	 int myArray[5];
+	 int remove_num,j,i;
+	 
+	 for(i=0;i<5;i++){
+       printf("Enter number %d = ",i+1);
+       scanf("%d",&myArray[i]);
+   }
+   printf("Which number you want to remove = ");
+   scanf("%d",&remove_num);
+   for(i=0;i<5;i++){
+   	if(remove_num == myArray[i]){
+	   	break;
+	   }
+   }
+   printf("i = %d \n",i);
+   if(i<5){
+   		for(j=0;j<=5-i;j++){
+		   	myArray[i] = myArray[i+1];
+		   	i++;
+	    }
+   }
+   printf("New array = {");
+   for(i=0;i<4;i++){
+   	printf("%d,",myArray[i]);
+   }
+   printf("}");
+   
+   
+}
