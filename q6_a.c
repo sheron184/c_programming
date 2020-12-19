11581a7558ded61bdee5fdd4bd2784770fd179ae
sheron jude <53206494+sheron184@users.noreply.github.com>
2020-12-19 22:04:59 +0530

    update q6_A

--- q6_a.c
+++ q6_a.c
@@ -14,31 +14,8 @@ int main(){
   		odds++;
   	}
   }
-  int oddNumbers[odds];
-  int evenNumbers[evens];
-  evens=0;
-  odds=0;
-  for(i=0;i<5;i++){
-  	if(myArray[i]%2 == 0){
-	  	evenNumbers[evens] = myArray[i];
-	  	evens++;
-	  }else{
-  		oddNumbers[odds] = myArray[i];
-  		odds++;
-  	}
-  }
-  lenEven = sizeof(evenNumbers)/sizeof(evenNumbers[0]);
-  lenOdd = sizeof(oddNumbers)/sizeof(oddNumbers[0]);
-  printf("Even numbers => ");
-  for(i=0;i<lenEven	;i++){
-			printf(" %d ",evenNumbers[i]);
-		}
-		printf("\n");
-		printf("Odd numbers => ");
-  for(i=0;i<lenOdd	;i++){
-			printf(" %d ",oddNumbers[i]);
-		}
-		printf("\n");
+  printf("There are %d even numbers \n",evens);
+  printf("There are %d odd numbers \n",odds);
   
    
 }
